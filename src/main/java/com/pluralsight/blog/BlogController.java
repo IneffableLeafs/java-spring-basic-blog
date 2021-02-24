9251b4e69d1da2233ce24ece0e48f9226cb96422
Peter Twarecki <64752041+IneffableLeafs@users.noreply.github.com>
2021-02-24 09:51:08 -0500

    Completed module

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -2,7 +2,9 @@ package com.pluralsight.blog;
 
 import com.pluralsight.blog.data.PostRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import com.pluralsight.blog.model.Post;
 
@@ -22,6 +24,13 @@ public class BlogController {
 
         public BlogController(PostRepository p) {
             postRepository = p;
+        }
+
+        @RequestMapping("/post/{id}")
+        public String postDetails(@PathVariable Long id, ModelMap modelMap) {
+            Post p = postRepository.findById(id);
+            modelMap.put("post", p);
+            return "post-details"; // the name of the template to display
         }
 
 }
